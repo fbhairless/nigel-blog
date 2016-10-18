0c3bcf1fed4f07749efa3c9052b9ddcb95565fc5
Hal Fairless and Nigel Burnett <hal@>
2016-10-18 11:41:16 -0700

    Add light and dark themes with button

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-theme");
+  });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-theme");
+  });
+
+
+
+
+
+
+});
